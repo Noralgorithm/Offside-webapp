2ba8eda18d562a2a6a4a39f49cebed1f8a375947
GloodBuster <josevglod@gmail.com>
2022-11-24 22:05:12 -0400

    Ver anuncio acoplado con ver cromos
    Todo es con imágenes de prueba para poder ver como se va viendo cada cosa

--- src/cores/get-chromes/GetChromes.jsx
+++ src/cores/get-chromes/GetChromes.jsx
@@ -2,29 +2,31 @@ import React from "react";
 import ChromesScreen from "./ChromesScreen";
 import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import WatchAds from "./WatchAds";
 
 function GetChromes({ hideDailyPack }) {
-  const [useChromesScreen, setUseChromesScreen] = useState(false);
+  const [watchAd, setWatchAd] = useState(false);
+  const [useChromeScreen, setUseChromeScreen] = useState(false);
   const [availablePack, setAvailablePack] = useState(true);
 
   return (
     <div className="w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-80">
       <div
         className={`w-full h-full flex justify-center items-center ${
-          useChromesScreen ? "hidden" : ""
+          watchAd || useChromeScreen ? "hidden" : ""
         }`}
       >
         <div className="w-1/2 h-2/3 bg-white rounded">
-          <div className="w-full flex justify-end">
-            <button className="py-1 px-2">
-              <AiOutlineClose
-                size="2rem"
-                onClick={() => {
-                  hideDailyPack(false);
-                }}
-              />
+          <header className="w-full flex justify-end">
+            <button
+              className="py-1 px-2"
+              onClick={() => {
+                hideDailyPack(false);
+              }}
+            >
+              <AiOutlineClose size="2rem" />
             </button>
-          </div>
+          </header>
           <div className="w-full h-1/2 flex justify-center items-center">
             <div className="w-1/3 h-2/5 relative flex items-center">
               <img
@@ -34,7 +36,7 @@ function GetChromes({ hideDailyPack }) {
                   availablePack ? "opacity-100" : "opacity-70"
                 }`}
                 onClick={() => {
-                  setUseChromesScreen(!useChromesScreen);
+                  setWatchAd(!watchAd);
                 }}
               />
             </div>
@@ -49,8 +51,14 @@ function GetChromes({ hideDailyPack }) {
           </div>
         </div>
       </div>
-      {useChromesScreen && (
-        <ChromesScreen hideChromesScreen={setUseChromesScreen} />
+      {watchAd && (
+        <WatchAds
+          adScreen={setWatchAd}
+          showChromesScreen={setUseChromeScreen}
+        />
+      )}
+      {useChromeScreen && (
+        <ChromesScreen hideChromesScreen={setUseChromeScreen} />
       )}
     </div>
   );
