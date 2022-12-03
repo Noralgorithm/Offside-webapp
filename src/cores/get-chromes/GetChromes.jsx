ea3315eb4d35d22c7ac5fb22903bcd58df5eb910
Chocopaula <mariapforero2003@gmail.com>
2022-12-03 00:36:19 -0400

    Insertar sobre offside
    Se insertó el sobre oficial de offside para las barajitas

--- src/cores/get-chromes/GetChromes.jsx
+++ src/cores/get-chromes/GetChromes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ChromesScreen from "./ChromesScreen";
+import SobreOffside from "../../Images/SobreOffside.png";
 import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import WatchAds from "./WatchAds";
@@ -31,8 +32,8 @@ function GetChromes({ hideDailyPack }) {
           <div className="w-full h-1/2 flex justify-center items-center">
             <div className="w-1/3 h-2/5 relative flex items-center">
               <img
-                src="https://s3-alpha-sig.figma.com/img/bc17/b2b5/5a233898015fdba1d2a19b413de2daa7?Expires=1669593600&Signature=Z~GLoX20C6n~QfoJblqSieQZ1Jm06DFPzoajlsFQiL1lG1lyc0ZrzKfg0q5q4Pt0~NG1FR-mqj5AUJBHobT2twIZWsPFeHkJju6T8~R8YRHxuceHDoHsPuNTQf38SGqreNfCQ-O86yfqVWVWKYJYJIuhYEVY353YI5F7eoFTtk~e8tx9NUwSxflMvUsKLubflrTAEh0KCFOwHnXhe-YlAe0vyqz7Ir2MyoGEIQiCyEWMo1IcVvbsqKDls8n4GgZnctYUe446Rxy0MbBcvI1~NlZZNJv-1V-5SdroOSNl~q-5vxRoU0lBpjHkPRT6jACKqxGHMCQrijLyNfGftyQh4Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-                alt=""
+                src={SobreOffside}
+                alt="Sobre Offside"
                 className={`hover:cursor-pointer object-contain ${
                   availablePack ? "opacity-100" : "opacity-70"
                 }`}
