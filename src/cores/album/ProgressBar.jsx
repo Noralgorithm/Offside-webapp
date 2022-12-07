db6e114a1844c54f3dd1bd25175d4a929e1ded16
GloodBuster <josevglod@gmail.com>
2022-12-06 21:21:59 -0400

    Arreglo en la barra de progreso

--- src/cores/album/ProgressBar.jsx
+++ src/cores/album/ProgressBar.jsx
@@ -3,13 +3,14 @@ import React from "react";
 function ProgressBar() {
   return (
     <div className="w-full flex justify-center h-full">
-      <div className="w-2/6 bg-gray-200 rounded-full h-full">
+      <p className="mr-8 font-bold text-xl">100%</p>
+      <div className="w-2/6 bg-stone-400 rounded-full h-full">
         <div
           className="bg-offsideColorWine h-full rounded-full flex items-center text-lg text-white font-semibold"
           style={{
-            width: "45%",
+            width: "10%",
           }}
-        ><p className="m-3">45%</p></div>
+        ></div>
       </div>
     </div>
   );
