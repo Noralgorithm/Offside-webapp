fdc5c244409ae452085d16d6c5c29d6ada837158
GloodBuster <josevglod@gmail.com>
2022-12-12 19:54:19 -0400

    mini refactor de componentes y navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,12 +20,16 @@ import { useLocation } from "react-router-dom";
 
 function Navbar() {
   const [showDailyPack, setShowDailyPack] = useState(false);
-  const [avalaiblePack, setAvailablePack] = useState(false);
+  const [avalaiblePack, setAvailablePack] = useState(true);
   const location = useLocation().pathname;
 
   return (
     <div className="w-screen">
-      {showDailyPack && <GetChromes hideDailyPack={setShowDailyPack} />}
+      {showDailyPack && (
+        <GetChromes
+          hideDailyPack={setShowDailyPack}
+        />
+      )}
       <nav className="w-full bg-white h-12 flex justify-between overflow-hidden">
         <Link className="h-full flex items-center" to="/">
           <img src={logoOffside} alt="" className="h-36" />
@@ -34,32 +38,32 @@ function Navbar() {
           <div className="flex h-full w-1/2 items-center justify-evenly">
             <Link className="rounded-full hover:bg-zinc-300 p-1">
               {location === "/album" ? (
-                <IoBook size="2rem" color="#63130B" />
+                <IoBook size="2rem" color="#B02419" />
               ) : (
-                <IoBookOutline size="2rem" color="#63130B" />
+                <IoBookOutline size="2rem" color="grey" />
               )}
             </Link>
             <Link to="/fantasy" className="rounded-full hover:bg-zinc-300 p-1">
               {location === "/fantasy" ||
               location === "/fantasy/plantilla" ||
               location === "/fantasy/almacen" ? (
-                <IoFootball size="2rem" color="#63130B" />
+                <IoFootball size="2rem" color="#B02419" />
               ) : (
-                <IoFootballOutline size="2rem" color="#63130B" />
+                <IoFootballOutline size="2rem" color="grey" />
               )}
             </Link>
             <Link className="rounded-full hover:bg-zinc-300 p-1">
               {location === "/tienda" ? (
-                <IoBasket size="2rem" color="#63130B" />
+                <IoBasket size="2rem" color="#B02419" />
               ) : (
-                <IoBasketOutline size="2rem" color="#63130B" />
+                <IoBasketOutline size="2rem" color="grey" />
               )}
             </Link>
             <Link className="rounded-full hover:bg-zinc-300 p-1">
               {location === "/perfil" ? (
-                <IoPerson size="2rem" color="#63130B" />
+                <IoPerson size="2rem" color="#B02419" />
               ) : (
-                <IoPersonOutline size="2rem" color="#63130B" />
+                <IoPersonOutline size="2rem" color="grey" />
               )}
             </Link>
           </div>
@@ -72,7 +76,7 @@ function Navbar() {
             <button
               className="hover:cursor-pointer rounded-full hover:bg-zinc-300 p-1 relative"
               onClick={() => {
-                setShowDailyPack(true);
+                setShowDailyPack(!showDailyPack);
               }}
             >
               {avalaiblePack && (
@@ -82,9 +86,9 @@ function Navbar() {
                 </div>
               )}
               {showDailyPack ? (
-                <IoGift size="2rem" color="#63130B" />
+                <IoGift size="2rem" color="#B02419" />
               ) : (
-                <IoGiftOutline size="2rem" color="#63130B" />
+                <IoGiftOutline size="2rem" color="grey" />
               )}
             </button>
           </div>
---
+++ src/cores/fantasy/Fantasy.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import cancha from "../../Images/cancha-horizontal.png";
+import { Link } from "react-router-dom";
+
+function Fantasy() {
+  return (
+    <div className="w-5/12 h-[90%] flex flex-col items-center">
+      <h2 className="text-3xl text-sticker-name font-bold">Fantasy</h2>
+      <div className="w-full h-5/6 bg-fondo-homepage-fantasy bg-no-repeat bg-contain flex flex-col items-center justify-center">
+        <h3 className="text-sticker-name font-semibold bg-gray-100 p-2 rounded-full w-[35%] text-center border-sticker-name border-2">
+          Mi puntaje: 45pts
+        </h3>
+        <Link className="w-[45%] h-[20%] flex bg-gray-100 items-center justify-around rounded border-2 border-sticker-name mt-4">
+          <img src={cancha} alt="" className="h-full" />
+          <h3 className="text-center text-sticker-name font-semibold">
+            Ver alineación de <br /> tu equipo
+          </h3>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Fantasy;
