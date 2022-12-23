069d863ec141d35e32068e915f77f317be60b5c1
GloodBuster <josevglod@gmail.com>
2022-12-22 21:33:53 -0400

    desplegar album con pega tus cromos

--- src/cores/album/PreAlbum.jsx
+++ src/cores/album/PreAlbum.jsx
@@ -13,9 +13,9 @@ function PreAlbum() {
   const [loading, setLoading] = useState(true);
   const [albumInfo, setAlbumInfo] = useState({});
 
-  const token = useSelector(state => state.user.token);
-  const eventId = useSelector(state => state.album.eventId)
-  const album = useSelector(state => state.album)
+  const token = useSelector((state) => state.user.token);
+  const eventId = useSelector((state) => state.album.eventId);
+  const album = useSelector((state) => state.album);
 
   useEffect(() => {
     (async () => {
@@ -23,8 +23,8 @@ function PreAlbum() {
       setAlbumInfo(data);
       setLoading(false);
     })();
-  }, [token, eventId, album.claimedSticker])
-  if (loading) return <h1>Loading...</h1>
+  }, [token, eventId, album.claimedSticker]);
+  if (loading) return <h1>Loading...</h1>;
   return (
     <div className="w-screen flex justify-around h-full">
       <div className="w-3/12 h-full flex items-center">
@@ -32,9 +32,9 @@ function PreAlbum() {
       </div>
       <div className="w-7/12 flex flex-col justify-evenly h-full">
         <div className="w-full h-[5%]">
-          <ProgressBar percentage={albumInfo.actualProgressPercentage}/>
+          <ProgressBar percentage={albumInfo.actualProgressPercentage} />
         </div>
-        
+
         {showAlbum ? (
           <Album />
         ) : (
@@ -53,7 +53,12 @@ function PreAlbum() {
           {showAlbum ? (
             <Carousel />
           ) : (
-            <div className="w-full h-full flex justify-center items-center">
+            <div
+              className="w-full h-full flex justify-center items-center cursor-pointer"
+              onClick={() => {
+                setShowAlbum(true);
+              }}
+            >
               <h1 className="h-full w-1/2 bg-gray-100 rounded-xl text-sticker-name flex justify-center items-center text-3xl font-semibold">
                 ¡Pega tus cromos!
               </h1>
