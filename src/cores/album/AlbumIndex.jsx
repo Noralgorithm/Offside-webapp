3d51ba77bada008de147de5e7216b17cbf48b3c9
GloodBuster <josevglod@gmail.com>
2022-12-23 17:26:21 -0400

    indice bien hermoso

--- src/cores/album/AlbumIndex.jsx
+++ src/cores/album/AlbumIndex.jsx
@@ -1,33 +1,59 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { filterTeam } from "../../features/album/albumSlice";
+import Select from "react-select";
+import { RiArrowDownSFill } from "react-icons/ri";
 
 function AlbumIndex() {
   const teamsList = useSelector((state) => state.album.teamsList);
   const dispatch = useDispatch();
+  console.log(teamsList);
 
   const handleChange = async (e) => {
-    if (e.target.value !== "") {
-        dispatch(filterTeam(e.target.value));
-        e.target.value = '';
-      }
+    console.log(e.value);
+    if (e.value !== "") {
+      dispatch(filterTeam(e.value));
+      e.value = "";
     }
+  };
+  const renderCustomItem = (item) => {
+    return (
+      <div className="w-full h-full flex gap-3">
+        <img src={item.image} alt="" className="h-5 w-7 rounded-full" />{" "}
+        <h1 className="text-black font-semibold">{item.label}</h1>
+      </div>
+    );
+  };
+  const DropdownIndicator = () => {
+    return <RiArrowDownSFill size="2rem" />;
+  };
 
   return (
-    <div className="w-full h-4/5">
-      <select
-        name=""
-        id=""
+    <div className="w-full h-4/5 bg-[#EFEFEF] rounded p-2">
+      <Select
+        value=""
+        options={teamsList.map((team) => {
+          console.log(team);
+          return { value: team.id, label: team.name, image: team.badge };
+        })}
+        formatOptionLabel={renderCustomItem}
+        className=""
         onChange={handleChange}
-        className="appearance-none w-full text-center rounded h-[8%] bg-sticker-name text-gray-100 border-none block font-semibold text-lg"
-      >
-        <option value="">Filtrar por equipos</option>
-        {teamsList.map((team) => (
-          <option value={team.id} key={team.id}>
-            {team.name}
-          </option>
-        ))}
-      </select>
+        menuIsOpen={true}
+        components={{ DropdownIndicator }}
+        placeholder="Equipos"
+        styles={{
+          control: (baseStyles, props) => ({
+            ...baseStyles,
+            backgroundColor: "#7CABAC",
+          }),
+          placeholder: (baseStyles, props) => ({
+            ...baseStyles,
+            color: "white",
+            fontWeight: 600,
+          }),
+        }}
+      />
     </div>
   );
 }
