4b881aa9cf3979347ab083920220f7ee1d9913aa
NØRA <joseandresrs0407@gmail.com>
2023-01-04 19:22:26 -0400

    Fixed filters
    Ahora se reinician a vacío los filtros al desmontarse el componente de la banca

--- src/cores/fantasy/bench/Bench.jsx
+++ src/cores/fantasy/bench/Bench.jsx
@@ -7,6 +7,7 @@ import {
   selectPlayer,
   storeBenchInfo,
   storeTeamList,
+  resetFilters
 } from "../../../features/fantasy/fantasySlice";
 import * as teamServices from "../../../services/team.services";
 
@@ -41,6 +42,10 @@ function Bench() {
         alert(e.message);
       }
     })();
+
+    return () => {
+      dispatch(resetFilters())
+    }
   }, [
     token,
     eventId,
