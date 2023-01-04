e814b5d49d572ae338fddf8c3970a192c27c6eef
GloodBuster <josevglod@gmail.com>
2023-01-04 14:49:56 -0400

    ya no se rueda la flecha cuando desaparece

--- src/cores/album/AlbumHeader.jsx
+++ src/cores/album/AlbumHeader.jsx
@@ -15,42 +15,44 @@ function AlbumHeader() {
 
   const isInLastPage =
     album.currentTeam.index === album.teamsList.length - 1 &&
-    (album.currentTeam.currentPage === album.currentTeam.pages || album.currentTeam.pages === 0);
+    (album.currentTeam.currentPage === album.currentTeam.pages ||
+      album.currentTeam.pages === 0);
 
-  const isInFirstPage = album.currentTeam.index === 0 && album.currentTeam.currentPage === 1;
+  const isInFirstPage =
+    album.currentTeam.index === 0 && album.currentTeam.currentPage === 1;
 
   return (
     <header className="h-full w-full flex items-center justify-around text-gray-100">
-      {!isInFirstPage && (
-        <IoIosArrowBack
-          size="2rem"
-          className="cursor-pointer rounded-full hover:bg-black hover:bg-opacity-40"
-          onClick={() => {
-            if (
-              album.currentTeam.currentPage === 1 ||
-              album.currentTeam.currentPage === 0
-            )
-              dispatch(setPrevTeam());
-            else dispatch(setPrevPage());
-          }}
-        />
-      )}
+      <IoIosArrowBack
+        size="2rem"
+        className={`cursor-pointer rounded-full hover:bg-black hover:bg-opacity-40 ${
+          isInFirstPage ? "invisible" : "visible"
+        }`}
+        onClick={() => {
+          if (
+            album.currentTeam.currentPage === 1 ||
+            album.currentTeam.currentPage === 0
+          )
+            dispatch(setPrevTeam());
+          else dispatch(setPrevPage());
+        }}
+      />
       <p className="text-xl font-semibold">{album.currentTeam.name}</p>
       <StickersCounter />
-      {!isInLastPage && (
-        <IoIosArrowForward
-          size="2rem"
-          className="cursor-pointer rounded-full hover:bg-black hover:bg-opacity-40"
-          onClick={() => {
-            if (
-              album.currentTeam.currentPage === album.currentTeam.pages ||
-              album.currentTeam.pages === 0
-            )
-              dispatch(setNextTeam());
-            else dispatch(setNextPage());
-          }}
-        />
-      )}
+      <IoIosArrowForward
+        size="2rem"
+        className={`cursor-pointer rounded-full hover:bg-black hover:bg-opacity-40 ${
+          isInLastPage ? "invisible" : "visible"
+        }`}
+        onClick={() => {
+          if (
+            album.currentTeam.currentPage === album.currentTeam.pages ||
+            album.currentTeam.pages === 0
+          )
+            dispatch(setNextTeam());
+          else dispatch(setNextPage());
+        }}
+      />
     </header>
   );
 }
